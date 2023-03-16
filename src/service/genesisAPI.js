0873ad394fd0ad6c1f9c912347ae87661008c53a
AlekasndrHavryliuk <104818533+AlekasndrHavryliuk@users.noreply.github.com>
2023-03-16 19:48:04 +0200

    Update genesisAPI.js

--- src/service/genesisAPI.js
+++ src/service/genesisAPI.js
@@ -12,7 +12,7 @@ $privateHost.interceptors.request.use(config => {
 
 export const fetchAuth = async () => {
   const { data } = await axios.get(
-    '?http://api.wisey.app/api/v1/auth/anonymous?platform=subscriptions'
+    'http://api.wisey.app/api/v1/auth/anonymous?platform=subscriptions'
   );
   return data;
 };
@@ -21,14 +21,14 @@ export const fetchCourses = async () => {
   const {
     data: { courses },
   } = await $privateHost.get(
-    '?http://api.wisey.app/api/v1/core/preview-courses'
+    'http://api.wisey.app/api/v1/core/preview-courses'
   );
   return courses;
 };
 
 export const fetchCoursesById = async id => {
   const { data } = await $privateHost.get(
-    `?http://api.wisey.app/api/v1/core/preview-courses/${id}`
+    `http://api.wisey.app/api/v1/core/preview-courses/${id}`
   );
   return data;
 };
